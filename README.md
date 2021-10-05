aa334d91e1d124774bfb40d806f6f0e3554a2d0a
Cotidib <constanza.dibueno@gmail.com>
2021-10-05 15:55:58 -0300

    spinner

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # shop-cart
 Practice project made with React Js and React Hooks
+
+This is my version of the Cart project in ['Code 15 React Projects' course by John Smilga](https://youtu.be/a_7Z7C_JCyo).
+
+### Overview and Learnings
+
+Main React functionalities I learned: use reducer and use context.
+
+### Resources
+
+['React Spinners'](https://www.davidhu.io/react-spinners/)
+```
+npm install --save react-spinners
+```
+
+### Live Site
--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import { useGlobalContext } from './context'
 import Navbar from './Navbar'
 import Cart from './Cart'
 
+import { css } from "@emotion/react";
+import ClipLoader from "react-spinners/ClipLoader"
+const override = css`
+  display: block;
+  margin: 30vh auto;
+`;
 
 function App() {
   const {loading} = useGlobalContext();
    if (loading) {
     return (
       <div className='loading'>
-        <h1>Loading...</h1>
+        <ClipLoader color='black' loading={loading} css={override} size={150} />
       </div>
     )
   }
